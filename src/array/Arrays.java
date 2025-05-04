82d550aa6fb59823d40c08e28f47487dc94b4e3f
Yash <vajayash27092006@gmail.com>
2025-05-04 16:00:42 +0530

    array part 2

--- src/array/Arrays.java
+++ src/array/Arrays.java
@@ -4,6 +4,100 @@ import java.util.Scanner;
 
 public class Arrays {
 
+    public static void buyAndSellStocks(int[] prices) {
+        int bp = Integer.MAX_VALUE;
+        int maxProfit = 0;
+        for (int i=0; i< prices.length; i++) {
+            if (bp < prices[i]) {
+                maxProfit = Math.max(maxProfit,(prices[i] - bp));
+            }else {
+                bp = prices[i];
+            }
+        }
+        System.out.println("Max profit: "+ maxProfit);
+    }
+
+    public static void trappedRainwater(int[] height) {
+
+        int n = height.length;
+        int[] lmb = new int[n];
+        int[] rmb = new int[n];
+        int totalTrappedRainwater = 0;
+
+        lmb[0] = height[0];
+        for (int i=1; i<n; i++) {
+            lmb[i] = Math.max( height[i],lmb[i-1]);
+        }
+
+        rmb[n-1] = height[n-1];
+        for (int i=n-2; i>=0; i--) {
+            rmb[i] = Math.max( height[i], rmb[i+1]);
+        }
+
+        for (int i=0; i<n; i++) {
+            int waterLevel = Math.min(lmb[i],rmb[i]);
+            totalTrappedRainwater += waterLevel - height[i] ;
+        }
+
+        System.out.println("Total Trapped Rain Water: "+totalTrappedRainwater);
+    }
+
+    public static void printMaxSubArraysSumKadane(int[] arr) {
+        int currSum =0;
+        int maxSum = 0;
+
+        for (int i=0; i<arr.length; i++) {
+            currSum += arr[i];
+            if (currSum < 0 ){
+                currSum = 0;
+            }
+            if (maxSum < currSum) {
+                maxSum = currSum;
+            }
+        }
+
+        System.out.println("Max sum using kadane's algorithm: "+ maxSum);
+    }
+
+    public static void printMaxSubArraysSumPrefix(int[] arr) {
+        int[] prefix = new int[arr.length];
+        int maxSum = 0;
+
+        prefix[0] = arr[0];
+        // calculate prefix array
+        for (int i=1; i< prefix.length; i++) {
+            prefix[i] = prefix[i-1] + arr[i];
+        }
+
+        // calculate max subarray sum
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i; j < arr.length; j++) {
+                int currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+                if (currSum > maxSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+
+        System.out.println("Max Sub-array Sum using prefix is: "+maxSum);
+    }
+
+    public static void printMaxSubArraysSum(int[] arr) {
+        int maxSum =0;
+        for (int i=0; i<arr.length; i++) {
+            for (int j=i; j<arr.length; j++) {
+                int currSum = 0;
+                for (int k=i; k<=j; k++) {
+                        currSum += arr[k];
+                    }
+                if (currSum > maxSum) {
+                    maxSum = currSum;
+                }
+            }
+        }
+        System.out.println("Max Sub arrays sum: "+maxSum);
+    }
+
     public static void printSubArrays(int[] arr) {
         for (int i=0; i<arr.length; i++) {
             for (int j=i; j<arr.length; j++) {
@@ -185,6 +279,36 @@ public class Arrays {
         // Print Sub-arrays
         int[] arr = {2,4,6,8,10};
         printSubArrays(arr);
+*/
+
+/*
+        // print  Subarrays maximum Sum
+        int[] arr = {1,-2,6,-1,3};
+        printMaxSubArraysSum(arr);
+*/
+
+ /*
+        // print  Subarrays maximum Sum prefix array
+        int[] arr = {1,-2,6,-1,3};
+        printMaxSubArraysSumPrefix(arr);
+*/
+
+/*
+        // print  Subarrays maximum Sum prefix array
+            int[] arr = {1,-2,6,-1,3};
+            printMaxSubArraysSumKadane(arr);
+*/
+
+/*
+        // trapped rain water
+        int[] arr = {4, 2, 0, 6, 3, 2, 5};
+        trappedRainwater(arr);
+*/
+
+/*
+        // Best time to buy & sell stock
+        int[] arr = {7, 1, 5, 3, 6, 4};
+        buyAndSellStocks(arr);
 */
         sc.close();
     }
